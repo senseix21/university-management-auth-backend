f3f96584e76e443a00963a19fc0f456e2f2a7840
senseix21 <mahdix2118@protonmail.com>
2023-06-08 10:34:47 +0600

    testing

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,11 @@
-import dotenv from 'dotenv';
+import dotenv from 'dotenv'
 
 // dotenv.config();
-dotenv.config({ path: '../.env' });
-
+dotenv.config({ path: '../.env' })
 
 const config = {
-    port: process.env.PORT || '5000', // Provide a default value ('5000') if PORT is undefined
-    db_url: process.env.DB_URL,
-};
+  port: process.env.PORT || '5000', // Provide a default value ('5000') if PORT is undefined
+  db_url: process.env.DB_URL,
+}
 
-export default config;
+export default config
--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,20 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
-
-const port = config.port;
+const port = config.port
 
 async function main() {
-    try {
-        await mongoose.connect(config.db_url as string);
-        console.log(` :smile Connected to database successfully!`);
+  try {
+    await mongoose.connect(config.db_url as string)
+    console.log(` :smile Connected to database successfully!`)
 
-        app.listen(port, () => {
-            console.log('App listening on port', port);
-        });
-
-    } catch (error) {
-        console.error('Error connecting to Mongoose server', error);
-    }
+    app.listen(port, () => {
+      console.log('App listening on port', port)
+    })
+  } catch (error) {
+    console.error('Error connecting to Mongoose server', error)
+  }
 }
 
-main();
+main()
